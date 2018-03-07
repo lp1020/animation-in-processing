be1f19010a4281d9e206f2de5d4d3a8b16652359
Liana Penn <lianapenn@gmail.com>
2018-03-07 10:50:41 -0500

    Update animation.java

--- animation.java
+++ animation.java
@@ -1,36 +1,47 @@
-static final int NUM_LINES = 10;
+import processing.core.PApplet;
 
-float t;
+    public class MainApp extends PApplet{
+        public static void main(String[] args) {
+            PApplet.main("MainApp", args);
+        }
 
-void setup() {
-  background(20);
-  size(500, 500);
-  
-}
+        static final int NUM_LINES = 10;
 
-void draw() {
-  background(20);
-  stroke(200);
-  strokeWeight(5);
-  translate(width/2, height/2);
-  for (int i = 0; i < NUM_LINES; i++) {
-  line(x1(t + i), y1(t + i), x2(t + i), y2(t + i));
-  }
-  t+= 0.5;
-}
+        float t;
 
-float x1(float t) {
-  return sin(t / 10) * 100 + sin(t / 5) * 20;
-}
+        public void settings() {
+            size(500, 500);
+        }
 
-float y1(float t) {
-  return cos(-t / 10) * 100 + sin(t / 5) * 50;
-}
+        public void setup() {
+            background(20);
 
-float x2(float t) {
-  return sin(t / 10) * 200 + sin(t) * 2 + cos(t) * 10;
-}
+        }
 
-float y2(float t) {
-  return cos(t / 20) * 200 + cos(t / 12) * 20;
-}
+        public void draw() {
+            background(20);
+            stroke(200);
+            strokeWeight(5);
+            translate(width/2, height/2);
+            for (int i = 0; i < NUM_LINES; i++) {
+                line(x1(t + i), y1(t + i), x2(t + i), y2(t + i));
+            }
+            t+= 0.5;
+        }
+
+        float x1(float t) {
+            return sin(t / 10) * 100 + sin(t / 5) * 20;
+        }
+
+        float y1(float t) {
+            return cos(-t / 10) * 100 + sin(t / 5) * 50;
+        }
+
+        float x2(float t) {
+            return sin(t / 10) * 200 + sin(t) * 2 + cos(t) * 10;
+        }
+
+        float y2(float t) {
+            return cos(t / 20) * 200 + cos(t / 12) * 20;
+        }
+    }
